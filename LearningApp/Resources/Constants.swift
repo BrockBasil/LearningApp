9a1569597e526fe1ea3b5f2025618f50c32f4c3e
BrockBasil <bbasil@purdue.edu>
2021-09-08 05:01:33 -0400

    - Add module navigation methods to ContentModel.swift - Add Constants file - Add ContentView file - Add ContentDetailView file - Add ContentViewRow file - Add HomeView file - Add HomeViewRow file - Import AVKit framework into project

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Brock Basil on 9/8/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+    
+}
